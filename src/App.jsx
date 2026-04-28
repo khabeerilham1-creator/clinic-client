7f75395a72760d1c400c7a1ee89966dcd3c7e01c
Khabeer <khabeerilham1@gmail.com>
2026-04-28 12:00:50 +0500

    REMOVE ADMIN FINAL FIX

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,12 @@ import Patients from "./pages/Patients";
 import Visits from "./pages/Visits";
 import AFI from "./pages/AFI";
 
-// Existing modules
 import FIS from "./pages/FIS";
 import CIS from "./pages/CIS";
 import Checkup from "./pages/Checkup";
 import Reports from "./pages/Reports";
 import Invoice from "./pages/Invoice";
 import LVI from "./pages/LVI";
-
-// ❌ Admin REMOVED (important)
 
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -27,65 +24,17 @@ function App() {
 
         <Route path="/" element={<Login />} />
 
-        <Route path="/dashboard" element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        } />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/patients" element={<ProtectedRoute><Patients /></ProtectedRoute>} />
+        <Route path="/visits" element={<ProtectedRoute><Visits /></ProtectedRoute>} />
+        <Route path="/afi" element={<ProtectedRoute><AFI /></ProtectedRoute>} />
 
-        <Route path="/patients" element={
-          <ProtectedRoute>
-            <Patients />
-          </ProtectedRoute>
-        } />
-
-        <Route path="/visits" element={
-          <ProtectedRoute>
-            <Visits />
-          </ProtectedRoute>
-        } />
-
-        <Route path="/afi" element={
-          <ProtectedRoute>
-            <AFI />
-          </ProtectedRoute>
-        } />
-
-        <Route path="/fis" element={
-          <ProtectedRoute>
-            <FIS />
-          </ProtectedRoute>
-        } />
-
-        <Route path="/cis" element={
-          <ProtectedRoute>
-            <CIS />
-          </ProtectedRoute>
-        } />
-
-        <Route path="/checkup" element={
-          <ProtectedRoute>
-            <Checkup />
-          </ProtectedRoute>
-        } />
-
-        <Route path="/reports" element={
-          <ProtectedRoute>
-            <Reports />
-          </ProtectedRoute>
-        } />
-
-        <Route path="/invoice" element={
-          <ProtectedRoute>
-            <Invoice />
-          </ProtectedRoute>
-        } />
-
-        <Route path="/lvi" element={
-          <ProtectedRoute>
-            <LVI />
-          </ProtectedRoute>
-        } />
+        <Route path="/fis" element={<ProtectedRoute><FIS /></ProtectedRoute>} />
+        <Route path="/cis" element={<ProtectedRoute><CIS /></ProtectedRoute>} />
+        <Route path="/checkup" element={<ProtectedRoute><Checkup /></ProtectedRoute>} />
+        <Route path="/reports" element={<ProtectedRoute><Reports /></ProtectedRoute>} />
+        <Route path="/invoice" element={<ProtectedRoute><Invoice /></ProtectedRoute>} />
+        <Route path="/lvi" element={<ProtectedRoute><LVI /></ProtectedRoute>} />
 
       </Routes>
     </Router>
