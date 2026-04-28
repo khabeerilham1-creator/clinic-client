1b814c0ebd2ad99bc5c7a443eb97ae8d14fc1769
Khabeer <khabeerilham1@gmail.com>
2026-04-28 08:49:02 +0500

    add admin page

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -2,7 +2,7 @@ export default function Admin() {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Admin Module 🚧 Coming Soon</h1>
-      <p>Manage users, approvals, and roles here.</p>
+      <p>Manage users and roles here.</p>
     </div>
   );
 }
