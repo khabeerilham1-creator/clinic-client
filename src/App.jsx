665bb03ea08a3bae4cdd93426ab1fa0d410831b8
Khabeer <khabeerilham1@gmail.com>
2026-04-30 04:45:06 +0500

    add all modules pages

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,16 @@ import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Patients from "./pages/Patients";
 
+// NEW MODULES
+import Visits from "./pages/Visits";
+import AFI from "./pages/AFI";
+import FIS from "./pages/FIS";
+import CIS from "./pages/CIS";
+import Checkup from "./pages/Checkup";
+import Reports from "./pages/Reports";
+import Invoice from "./pages/Invoice";
+import LVI from "./pages/LVI";
+
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(
     !!localStorage.getItem("token")
@@ -14,41 +24,32 @@ function App() {
     <Router>
       <Routes>
 
-        {/* LOGIN */}
         <Route
           path="/"
           element={
-            isLoggedIn ? (
-              <Navigate to="/dashboard" />
-            ) : (
-              <Login setIsLoggedIn={setIsLoggedIn} />
-            )
+            isLoggedIn ? <Navigate to="/dashboard" /> : <Login setIsLoggedIn={setIsLoggedIn} />
           }
         />
 
-        {/* DASHBOARD */}
         <Route
           path="/dashboard"
-          element={
-            isLoggedIn ? (
-              <Dashboard />
-            ) : (
-              <Navigate to="/" />
-            )
-          }
+          element={isLoggedIn ? <Dashboard /> : <Navigate to="/" />}
         />
 
-        {/* PATIENTS */}
         <Route
           path="/patients"
-          element={
-            isLoggedIn ? (
-              <Patients setIsLoggedIn={setIsLoggedIn} />
-            ) : (
-              <Navigate to="/" />
-            )
-          }
+          element={isLoggedIn ? <Patients setIsLoggedIn={setIsLoggedIn} /> : <Navigate to="/" />}
         />
+
+        {/* NEW MODULE ROUTES */}
+        <Route path="/visits" element={<Visits />} />
+        <Route path="/afi" element={<AFI />} />
+        <Route path="/fis" element={<FIS />} />
+        <Route path="/cis" element={<CIS />} />
+        <Route path="/checkup" element={<Checkup />} />
+        <Route path="/reports" element={<Reports />} />
+        <Route path="/invoice" element={<Invoice />} />
+        <Route path="/lvi" element={<LVI />} />
 
       </Routes>
     </Router>
