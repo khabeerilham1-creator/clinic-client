f5c13e091e99cbfd7ca95587279e8f4991e16a13
Khabeer <khabeerilham1@gmail.com>
2026-04-29 05:21:20 +0500

    fix baseURL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://pis-backend-final-1.onrender.com/api/api/api"
+  baseURL: "https://pis-backend-final-1.onrender.com/api"
 });
 
 export default api;
