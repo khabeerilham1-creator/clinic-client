81f640751556e3afad01471a0c4ae2a71b7d242e
Khabeer <khabeerilham1@gmail.com>
2026-05-01 20:32:14 +0500

    fix invoice auth

--- src/api.js
+++ src/api.js
@@ -4,8 +4,11 @@ const api = axios.create({
   baseURL: "https://pis-backend-final-1.onrender.com"
 });
 
+// ✅ attach token
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
+
+  console.log("TOKEN:", token); // DEBUG
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
